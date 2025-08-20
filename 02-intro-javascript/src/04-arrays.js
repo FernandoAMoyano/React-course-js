3a296169c4abbcd4d1f1fb66ffa175f359ea0e82
FernandoAMoyano <fernandomoyano21@gmail.com>
2025-08-20 17:05:29 -0300

    arreglos

---
+++ 02-intro-javascript/src/04-arrays.js
@@ -0,0 +1,22 @@
+const miArreglo1 = new Array();
+
+miArreglo1.push(1);
+miArreglo1.push(2);
+miArreglo1.push(3);
+
+console.log(miArreglo1);
+
+/* -------------------------------------- */
+
+const miArreglo2 = [1, 2, 3, 4];
+//let miArreglo3 = miArreglo2;
+//miArreglo3.push(5);
+let miArreglo3 = [...miArreglo2, 5];
+
+const miArreglo4 = miArreglo3.map((numero) => {
+  numero * 2;
+});
+
+console.log(miArreglo2);
+console.log(miArreglo3);
+console.log(miArreglo4);
