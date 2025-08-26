aad3d91aa8332ec335ea528c08ec2d3e98820038
FernandoAMoyano <fernandomoyano21@gmail.com>
2025-08-26 12:29:43 -0300

    desestructuracion de arreglos

---
+++ 02-intro-javascript/src/07-desestructuracionArr.js
@@ -0,0 +1,31 @@
+const personajes = [ "Goku", "Vegeta", "Trunks" ];
+
+/* console.log(personajes[0]);
+console.log(personajes[1]);
+console.log(personajes[2]); */
+
+const [ , p2 ] = personajes;
+console.log( p2 );
+
+const retornaArreglo = () => {
+  return [ "ABC", 123 ];
+};
+
+const [ letras, numeros ] = retornaArreglo();
+console.log( letras, numeros );
+
+//Smulacion del hook useState
+const usarEstado = ( valor ) => {
+  return [
+    valor,
+    () => {
+      console.log( "Hola mundo" );
+    },
+  ];
+};
+
+const arr = usarEstado( "Goku" );
+console.log( arr );
+
+const [ nombre, setNombre ] = usarEstado( "Goku" )
+console.log( nombre, setNombre );
